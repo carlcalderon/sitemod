fbf2d75c2f39daa8e3f1cd474daace77f3bddb10
Carl Calderon <carl.calderon@gmail.com>
2020-01-15 00:58:12 +0100

    Initial commit

---
+++ src/Options.svelte
@@ -0,0 +1,40 @@
+<script>
+
+  console.log('hello world!')
+
+  // // Saves options to chrome.storage
+  // function save_options() {
+  //   var color = document.getElementById('color').value;
+  //   var likesColor = document.getElementById('like').checked;
+  //   chrome.storage.sync.set({
+  //     favoriteColor: color,
+  //     likesColor: likesColor
+  //   }, function() {
+  //     // Update status to let user know options were saved.
+  //     var status = document.getElementById('status');
+  //     status.textContent = 'Options saved.';
+  //     setTimeout(function() {
+  //       status.textContent = '';
+  //     }, 750);
+  //   });
+  // }
+
+  // // Restores select box and checkbox state using the preferences
+  // // stored in chrome.storage.
+  // function restore_options() {
+  //   chrome.storage.sync.get({
+  //     modifiers: JSON.stringify([]),
+  //   }, function(items) {
+  //     const modifiers = JSON.parse(items.modifiers)
+  //     document.getElementById('color').value = items.favoriteColor;
+  //     document.getElementById('like').checked = items.likesColor;
+  //   });
+  // }
+  // document.addEventListener('DOMContentLoaded', restore_options);
+  // document.getElementById('save').addEventListener('click',
+  // save_options);
+
+
+</script>
+
+<h1>Options • sitemod</h1>
