759f0c8d27f00bdcfb13611bb3c942621480d9a1
Carl Calderon <carl.calderon@gmail.com>
2020-01-16 21:25:43 +0100

    Move loading and saving to the Sitemod main component

--- src/Options.svelte
+++ src/Options.svelte
@@ -2,6 +2,7 @@
   import { fly } from 'svelte/transition'
   import ModEditor from './components/ModEditor.svelte'
 
+  export let save = null
   export let showBackButton = false
   export let modifiers = []
   let saved = false
@@ -15,52 +16,6 @@
   function showDetails (index) {
     selectedIndex = index
     showBackButton = true
-  }
-
-  function restore_options() {
-    console.log('options restored!')
-    chrome.storage.sync.get({
-      modifiers: JSON.stringify([
-    {
-      name: 'Twitch - Picture in picture',
-      pattern: 'twitch\\.tv',
-      scripts: ['console.log(\'twitch pattern matched!\')'],
-      styles: ['video { z-index: 999 }'],
-      enabled: true
-    },
-    {
-      name: 'YouTube - Full width',
-      pattern: 'youtube\\.com',
-      scripts: ['console.log(\'youtube pattern matched!\')'],
-      styles: ['body { background-color: red }'],
-      enabled: true
-    },
-    {
-      name: 'Google',
-      pattern: 'google\\.com',
-      scripts: ['console.log(\'google pattern matched!\')'],
-      styles: ['body { background-color: red }'],
-      enabled: false
-    }
-  ]),
-    }, function(items) {
-      modifiers = JSON.parse(items.modifiers)
-    });
-  }
-  document.addEventListener('DOMContentLoaded', restore_options)
-
-  function save () {
-    console.log('save')
-
-    chrome.storage.sync.set({
-      modifiers: JSON.stringify(modifiers)
-    }, function() {
-      // Update status to let user know options were saved.
-      saved = true
-      setTimeout(function() {
-        saved = false
-      }, 750);
-    });
   }
 
   function addMod () {
@@ -81,21 +36,21 @@
 
 <style>
   .modifier {
-    height: 40px;
-    border-bottom: 6px solid grey;
+    height: 65px;
+    border-bottom: 6px solid #F2F3F5;
     position: relative;
-    padding: 0px 15px;
+    padding: 0px 25px;
   }
 
   aside {
     position: absolute;
-    right: 15px;
+    right: 25px;
     top: 0;
-    padding: 12px 0px;
+    padding: 25px 0px;
   }
 
   .name {
-    padding: 12px 0px;
+    padding: 25px 0px;
     font-weight: bold;
     font-size: 1.1em;
   }
@@ -114,6 +69,9 @@
   .details {
     padding: 15px;
   }
+  h2 {
+    margin-top: 0px;
+  }
 </style>
 {#if selectedIndex === null}
   <section out:fly={{x: -100}} in:fly={{x: -100}}>
@@ -122,8 +80,8 @@
       <div class="modifier">
         <div class="name" on:click={() => showDetails(index)}>{mod.name}</div>
         <aside>
-          <input type="checkbox" bind:checked={mod.enabled} />
           <button on:click={() => removeModAt(index)}>Remove</button>
+          <input type="checkbox" bind:checked={mod.enabled} />
         </aside>
       </div>
     {/each}
--- src/Sitemod.svelte
+++ src/Sitemod.svelte
@@ -2,12 +2,36 @@
   import { fly } from 'svelte/transition'
   import Options from './Options.svelte'
 
+  let modifiers = []
   let options = null
   let showBackButton = false
 
   function goBack () {
     options.goBack()
   }
+
+  function load() {
+    chrome.storage.sync.get({
+      modifiers: '[]',
+    }, function(items) {
+      modifiers = JSON.parse(items.modifiers)
+    })
+  }
+
+
+  function save () {
+    chrome.storage.sync.set({
+      modifiers: JSON.stringify(modifiers)
+    }, function() {
+      // Update status to let user know options were saved.
+      saved = true
+      setTimeout(function() {
+        saved = false
+      }, 750)
+    })
+  }
+
+  document.addEventListener('DOMContentLoaded', load)
 
 </script>
 
@@ -22,11 +46,25 @@
   :global(body) {
     display: flex;
     flex-direction: column;
+    color: #505050;
+  }
+  :global(button) {
+    appearance: none;
+    padding: 3px 9px 4px 9px;
+    border: 1px solid #c7c7c7;
+    border-radius: 2px;
+    background: white;
+    color: #a0a0a0;
+    transition: background 200ms;
+  }
+  :global(button:hover) {
+    background: #eeeeee;
+    transition: none;
   }
   header {
     display: flex;
     justify-content: flex-end;
-    background: grey;
+    background: #F2F3F5;
     padding: 15px;
   }
   main {
@@ -50,5 +88,10 @@
   <h1>sitemod</h1>
 </header>
 <main>
-  <Options bind:this={options} bind:showBackButton={showBackButton}></Options>
+  <Options
+    bind:save
+    bind:modifiers
+    bind:this={options}
+    bind:showBackButton={showBackButton}
+  />
 </main>
