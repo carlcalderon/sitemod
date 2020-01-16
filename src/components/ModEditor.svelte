42fc440a9715c43c3f5a0de0bad311337df654b0
Carl Calderon <carl.calderon@gmail.com>
2020-01-16 21:23:28 +0100

    Reduce complexity of the ModEditor

--- src/components/ModEditor.svelte
+++ src/components/ModEditor.svelte
@@ -4,7 +4,6 @@
   export let pattern
   export let scripts = []
   export let styles = []
-  export let enabled = false
 
 </script>
 <style>
@@ -14,31 +13,20 @@
   input {
     display: block;
   }
-  textarea {
-    width: 100%;
-    height: 200px;
-  }
 </style>
 
-<label>Name
-  <input type="text" bind:value={name} placeholder="My sitemod" />
-</label>
-<label>Pattern
-  <input type="text" bind:value={pattern} placeholder="example\.com" />
-</label>
+<label>Name</label>
+<input type="text" bind:value={name} placeholder="My sitemod" />
+
+<label>Pattern</label>
+<input type="text" bind:value={pattern} placeholder="example\.com" />
 
 <h2>Scripts</h2>
 {#each scripts as script}
   <CodeEditor language='javascript' bind:value={script}></CodeEditor>
-  <button>Remove script</button>
 {/each}
-<button>Add script</button>
 
 <h2>Styles</h2>
 {#each styles as style}
   <CodeEditor language='css' bind:value={style}></CodeEditor>
-  <button>Remove style</button>
 {/each}
-<button>Add style</button>
-
-<input type="checkbox" bind:checked={enabled} />
